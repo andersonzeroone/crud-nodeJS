3cdd9069499c1607b40135424bb9ed2853023aae
Anderson Pablo <anderson.pablo02@gmail.com>
2022-02-25 14:09:02 -0300

    feat(index.js) - finish task

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const server = express();
+
+
+server.use(express.json());
+
+
+const cursos = ['React native', 'js', 'web'];
+
+server.get('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(cursos[index]);
+});
+
+
+server.get('/cursos', (req, res) => {
+  return res.json(cursos);
+});
+
+
+server.post('/cursos', (req, res) => {
+  const { name } = req.body;
+
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+
+
+server.put('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+
+  cursos[index] = name;
+
+  return res.json(cursos)
+
+});
+
+
+server.delete('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+
+  cursos.splice(index, 1);
+
+  return res.json({ message: 'Cursos deleted successfully' })
+
+});
+
+server.listen(3000);
